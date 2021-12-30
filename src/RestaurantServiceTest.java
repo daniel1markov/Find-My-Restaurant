987fd3be5ed72f6fd6003123f96f9f2fd7a19936
daniel <daniel1markov@gmail.com>
2021-12-30 13:57:45 +0200

    test is in src now.

---
+++ src/RestaurantServiceTest.java
@@ -0,0 +1,19 @@
+import com.hit.algorithm.IAlgoPatternSearching;
+import com.hit.algorithm.RabinKarpAlgorithm;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class RestaurantServiceTest {
+
+        static final String txt = "Daniel and Inbar Algorithms";
+
+        @Test
+        public void RabinKarpTest_PatternExists_ShouldPass() throws Exception {
+            String pat = "Algo";
+            IAlgoPatternSearching robinKarp = new RabinKarpAlgorithm(txt, pat);
+            boolean result = robinKarp.Search();
+
+        }
+}
