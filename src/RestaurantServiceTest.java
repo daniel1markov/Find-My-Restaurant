89d4b694522ccf1312870538aaa1c6aec27f2b64
daniel <daniel1markov@gmail.com>
2022-01-08 11:40:16 +0200

    updated Nissim's tasks.

--- src/RestaurantServiceTest.java
+++ src/RestaurantServiceTest.java
@@ -1,19 +1,36 @@
 import com.hit.algorithm.IAlgoPatternSearching;
 import com.hit.algorithm.RabinKarpAlgorithm;
+import com.hit.dao.DaoRestaurantImpl;
+import com.hit.dm.Restaurant;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class RestaurantServiceTest {
 
-        static final String txt = "Daniel and Inbar Algorithms";
+    @Test
+    public void GetCategoriesService_FindSushiRests_ShouldReturn2() throws IOException, ClassNotFoundException {
 
-        @Test
-        public void RabinKarpTest_PatternExists_ShouldPass() {
-            String pat = "Algo";
-            IAlgoPatternSearching robinKarp = new RabinKarpAlgorithm(txt, pat);
-            boolean result = robinKarp.Search();
+        DaoRestaurantImpl daoRestaurant = new DaoRestaurantImpl();
+        Map <String, List <Restaurant>> categories = daoRestaurant.findByCategory();
 
-        }
+        List<Restaurant> sushiRests = categories.get("Sushi");
+
+        Assert.assertTrue(sushiRests.size() == 2);
+    }
+
+    @Test
+    public void SaveNewRestService_FindNewNumberOfRests_ShouldReturn7() throws IOException, ClassNotFoundException {
+
+        DaoRestaurantImpl daoRestaurant = new DaoRestaurantImpl();
+        Restaurant restaurant = new Restaurant("test", "test", "test", "test", "test", "test");
+        daoRestaurant.save(restaurant);
+
+        List<Restaurant> allRests = daoRestaurant.findAll();
+
+        Assert.assertTrue(allRests.size() == 7);
+    }
 }
